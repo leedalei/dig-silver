63a98a34f54866ef5610917cd1358cd0b16ce51d
BianTan <biantan@outlook.com>
2020-11-27 18:18:15 +0800

    feat:新增 scroll 滚动指定位置

--- src/App.vue
+++ src/App.vue
@@ -4,10 +4,13 @@
       改变主题
     </button>
     <router-view />
+    <button class="fixed right-0 bottom-0 px-12 py-4 border-solid border-2 border-blue-400 rounded-full outline-none" @click="backTop">回到顶部</button>
   </div>
 </template>
 
 <script>
+import { scrollTo } from './common/utils'
+
 export default {
   name: "App",
   components: {},
@@ -28,8 +31,16 @@ export default {
         this.theme = "light"
       }
     },
+    backTop() {
+      const e = document.documentElement || document.body
+      scrollTo(e)
+    }
   },
 }
 </script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+  #app {
+    height: 1000px;
+  }
+</style>
---
+++ src/common/utils.js
@@ -0,0 +1,14 @@
+/**
+ * 滚动条转跳到指定位置
+*/
+export function scrollTo(element, to = 0, duration = 300) {
+  if (duration <= 0) return
+  const difference = to - element.scrollTop
+  const perTick = difference / duration * 10
+
+  setTimeout(() => {
+    element.scrollTop = element.scrollTop + perTick
+    if (element.scrollTop == to) return
+    scrollTo(element, to, duration - 10)
+  }, 10)
+}
